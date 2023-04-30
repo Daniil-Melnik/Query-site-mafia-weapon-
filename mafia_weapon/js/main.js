16f85abce24e6a44ae6e647e4c49e430b0c4fd1c
Daniil-Melnik <113520606+Daniil-Melnik@users.noreply.github.com>
2023-05-01 00:31:18 +0300

    Add files via upload

---
+++ mafia_weapon/js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+  //$(":checkbox").fadeOut(1000);
+  //$(":radio").fadeOut(1000);
+  //$(":submit").fadeOut(1000);
+  //$(":reset").fadeOut(1000);
+  //$(":file").fadeOut(1000);
+  $(":button").fadeOut(1000);
+  //$(':text').fadeOut(1000);
+  //$(':password').fadeOut(1000);
+  //$(':input').fadeOut(1000);
+  //$('.form1 : input').fadeOut(1000);
+});
